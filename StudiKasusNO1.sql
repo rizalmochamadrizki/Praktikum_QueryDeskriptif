cbfedcfac92883c45dd435f39ffd14b0e331cd01
rizalmochamadrizki <114121962+rizalmochamadrizki@users.noreply.github.com>
2022-10-22 18:48:25 +0700

    Add files via upload

---
+++ StudiKasusNO1.sql
@@ -0,0 +1,1 @@
+SELECT ig_username, COUNT(ig_username) AS total_content_2020 FROM tbl_scraping WHERE ig_username = "Surabaya" AND (taken_at BETWEEN '2020-01-01' AND '2021-01-01');
---
+++ StudiKasusNo2.sql
@@ -0,0 +1,2 @@
+SELECT tbl_scraping.`ig_username`, tbl_scraping.`follower_count`
+FROM tbl_scraping GROUP BY ig_username
